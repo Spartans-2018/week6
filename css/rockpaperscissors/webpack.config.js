2dc697fb34669c8e669fcb18040f1e444b37af8d
nikhilpanchal <nikhilpanchal@gmail.com>
2019-02-24 19:51:01 -0500

    Added material for CSS
    Describing CSS with links to MDN pages, along with an example to illustrate the use of css

---
+++ css/rockpaperscissors/webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const HtmlWebpackPluginConfig = new HtmlWebPackPlugin({
+    template: './src/index.html',
+    filename: './index.html',
+    inject: 'body'
+});
+
+module.exports = {
+    entry: [
+        './src/styles/app.css',
+        './src/js/index.js'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {},
+                    },
+                ],
+            }
+        ]
+    },
+    plugins: [
+        HtmlWebpackPluginConfig
+    ]
+};
