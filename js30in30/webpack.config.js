d97549d81ac4b5664b2beb47d82fcf942b884cab
nikhilpanchal <nikhilpanchal@gmail.com>
2019-03-30 09:22:00 -0400

    First Exercise: Drum kit from Js30" The drum kit one. Pretty interesting use of the audio tag and css transition

---
+++ js30in30/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const babelPolyfill = require("babel-polyfill");
+
+const HtmlWebpackPluginConfig = new HtmlWebPackPlugin({
+    template: './src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+module.exports = {
+    entry: [
+        'babel-polyfill',
+        './src/js/start.js',
+        './src/styles/app.css'
+    ],
+    devtool: 'eval-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|wav)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {},
+                    },
+                ],
+            }
+        ]
+    },
+    plugins: [
+        HtmlWebpackPluginConfig
+    ]
+};
