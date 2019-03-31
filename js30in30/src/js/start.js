9ac031892b3c485690fc43ac64e22f1bbeb965d8
nikhilpanchal <nikhilpanchal@gmail.com>
2019-03-30 21:33:23 -0400

    Exercises 1 and 2: Drums and Clock

--- js30in30/src/js/start.js
+++ js30in30/src/js/start.js
@@ -1,26 +1,27 @@
 
 
-addEventListener('keydown', (e) => {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+setInterval(secondsTickHandler, 1000);
 
-    if (!audio) {
-        return;
-    }
-    audio.currenTime = 0; // rewind it to the beginning to allow continuous playing
-    audio.play();
+/**
+ * Scales a number to a rotation degree based on the scale provided.
+ * 
+ * @param {*} seconds 
+ * @param {*} scale 
+ */
+function convertToRotateDegrees(seconds, scale) {
+    return (seconds / scale * 360);
+}
 
-    const div = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    div.classList.add('playing');
-}, false);
+function secondsTickHandler() {
+    const date = new Date();
 
-let removeStyle = (e) => {
-    if (e.propertyName !== 'transform') {
-        return;
-    }
+    rotateElement(date.getSeconds(), 60, '.second-hand');
+    rotateElement(date.getMinutes(), 60, ".minute-hand");
+    rotateElement(date.getHours(), 12, ".hour-hand");
+}
 
-    const div = e.currentTarget;
-    div.classList.remove('playing');
-};
-
-const keys = document.querySelectorAll(".key")
-keys.forEach(key => key.addEventListener('transitionend', removeStyle));
+function rotateElement(timeSegment, rotationScale, elementCSSSelector) {
+    const degrees = convertToRotateDegrees(timeSegment, rotationScale);
+    document.querySelector(elementCSSSelector)
+        .style.transform = `rotate(${degrees + 90}deg)`;
+}
