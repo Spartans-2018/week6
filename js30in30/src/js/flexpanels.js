d3b8aed44b8641523d43003f02afe0680c356a0a
nikhilpanchal <nikhilpanchal@gmail.com>
2019-04-09 20:50:54 -0400

    Only have one panel active at a time

--- js30in30/src/js/flexpanels.js
+++ js30in30/src/js/flexpanels.js
@@ -1,12 +1,19 @@
 function toggleOpen() {
+    // remove it from any other panel that is open
+    let otherPanel = document.querySelector(".open");
+    if (otherPanel && otherPanel !== this) {
+        otherPanel.classList.toggle('open');
+    }
+
     this.classList.toggle('open');
 }
 
 function toggleOpenActive(e) {
     if (e.propertyName === 'flex-grow') {
-        this.classList.toggle('open-active');
+        this.classList.toggle('open-active')
     }
 }
+
 
 document.querySelectorAll('.panel')
         .forEach(panel => {
